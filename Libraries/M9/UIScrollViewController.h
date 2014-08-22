69e346a4ae8caef635278cca7237528f84484ef4
MingLQ <minglq.9@gmail.com>
2014-08-22 19:49:34 +0800

    UIScrollViewController

---
+++ Libraries/M9/UIScrollViewController.h
@@ -0,0 +1,13 @@
+//
+//  UIScrollViewController.h
+//  M9Dev
+//
+//  Created by MingLQ on 2014-08-22.
+//  Copyright (c) 2014年 iwill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScrollViewController : UIViewController
+
+@end
---
+++ Libraries/M9/UIScrollViewController.m
@@ -0,0 +1,37 @@
+//
+//  UIScrollViewController.m
+//  M9Dev
+//
+//  Created by MingLQ on 2014-08-22.
+//  Copyright (c) 2014年 iwill. All rights reserved.
+//
+
+#import "UIScrollViewController.h"
+
+@interface UIScrollViewController ()
+
+@end
+
+@implementation UIScrollViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
