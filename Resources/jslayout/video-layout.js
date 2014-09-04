d2ec58dec0dfee5987488e3e5ec1ceec0451f834
iwill <minglq.9@gmail.com>
2014-09-05 02:36:53 +0800

    不用 this.width & this.height

--- Resources/jslayout/video-layout.js
+++ Resources/jslayout/video-layout.js
@@ -27,21 +27,20 @@
         constructor: function() {
         },
         name: "JSLayout",
-        width: 0,
-        height: 0,
         placeholderImage: "iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAACXBIWXMAABYlAAAWJQFJUiTwAAAAHGlET1QAAAACAAAAAAAAAAEAAAAoAAAAAQAAAAEAAABDeWVRjwAAAA9JREFUCB1iYGBgaABhAAAAAP//k71SXwAAAAxJREFUY2BgYGgAYQAPEgIBCnYZtgAAAABJRU5ErkJggg==",
         layout_setUp: function(layout) {
-            layout.itemSize = { width: this.width, height: this.height };
             layout.js_sectionInset = { top: 7, left: 7, bottom: 7, right: 7 };
             layout.minimumLineSpacing = 6;
             layout.minimumInteritemSpacing = 6;
         },
         cell_setUp: function(cell) {
-            var imageView = new UIImageView({ x: 0, y: 0, width: this.width, height: this.height });
+            var width = cell.contentView.bounds.width, height = cell.contentView.bounds.height;
+            
+            var imageView = new UIImageView({ x: 0, y: 0, width: width, height: height });
             imageView.autoresizingMask = (UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight);
             
             var lableHeight = fontSize + textMargin * 2;
-            var titleLabel = new UILabel({ x: 0, y: this.height - lableHeight, width: this.width, height: lableHeight });
+            var titleLabel = new UILabel({ x: 0, y: height - lableHeight, width: width, height: lableHeight });
             titleLabel.textAlignment = NSTextAlignmentCenter;
             titleLabel.font = UIFont.systemFontOfSize(fontSize);
             titleLabel.backgroundColor = UIColor.colorWithHexString("#FFFFFF").colorWithAlphaComponent(0.93);
@@ -65,8 +64,10 @@
     
     var JSHorLayout = $class({
         name: "JSHorLayout",
-        width: 150,
-        height: 124,
+        layout_setUp: function(layout) {
+            JSLayout.prototype.layout_setUp.apply(this, arguments);
+            layout.itemSize = { width: 150, height: 124 };
+        },
         cell_updateWithData: function(cell, data) {
             JSLayout.prototype.cell_updateWithData.apply(this, arguments);
             var url = NSURL.URLWithString(data.hor_high_pic);
@@ -77,8 +78,10 @@
     
     var JSVerLayout = $class({
         name: "JSVerLayout",
-        width: 98,
-        height: 135,
+        layout_setUp: function(layout) {
+            JSLayout.prototype.layout_setUp.apply(this, arguments);
+            layout.itemSize = { width: 98, height: 135 };
+        },
         cell_updateWithData: function(cell, data) {
             JSLayout.prototype.cell_updateWithData.apply(this, arguments);
             var url = NSURL.URLWithString(data.ver_high_pic);
