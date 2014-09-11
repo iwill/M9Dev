67745c0e473127fe57cfd4299a84bfcbefca27f4
iwill <minglq.9@gmail.com>
2014-09-11 23:03:41 +0800

    JS Managed References

--- M9Dev/JSCollectionViewFlowLayout.m
+++ M9Dev/JSCollectionViewFlowLayout.m
@@ -10,25 +10,39 @@
 
 #import "JSCore.h"
 
-@implementation JSCollectionViewFlowLayout
+@interface JSCollectionViewFlowLayout ()
+
+@end
+
+@implementation JSCollectionViewFlowLayout {
+    JSManagedValue *_jsLayout;
+}
+
+@dynamic jsLayout;
+
+- (JSValue *)jsLayout {
+    return _jsLayout.value;
+}
 
 - (void)setJsLayout:(JSValue *)jsLayout {
     _name = [jsLayout[@"name"] toString];
-    _jsLayout = jsLayout;
+    // !!!: JSManagedValue & Managed References - https://developer.apple.com/videos/wwdc/2013/?id=615
+    _jsLayout = [JSManagedValue managedValueWithValue:jsLayout];
+    [jsLayout.context.virtualMachine addManagedReference:_jsLayout withOwner:self];
     
-    [_jsLayout callMethod:@"layout_setUp" withArguments:@[ self ]];
+    [_jsLayout.value callMethod:@"layout_setUp" withArguments:@[ self ]];
 }
 
 - (void)cell_setUp:(JSCollectionViewCell *)cell {
-    [_jsLayout callMethod:@"cell_setUp" withArguments:@[ cell ]];
+    [_jsLayout.value callMethod:@"cell_setUp" withArguments:@[ cell ]];
 }
 
 - (void)cell_prepareForReuse:(JSCollectionViewCell *)cell {
-    [_jsLayout callMethod:@"cell_prepareForReuse" withArguments:@[ cell ]];
+    [_jsLayout.value callMethod:@"cell_prepareForReuse" withArguments:@[ cell ]];
 }
 
 - (void)cell_update:(JSCollectionViewCell *)cell withData:(NSData *)data {
-    [_jsLayout callMethod:@"cell_updateWithData" withArguments:@[ cell, data ]];
+    [_jsLayout.value callMethod:@"cell_updateWithData" withArguments:@[ cell, data ]];
 }
 
 @end
