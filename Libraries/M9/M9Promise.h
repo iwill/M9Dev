33c26f443eab835670b10efbc94fec0dee9217ce
MingLQ <minglq.9@gmail.com>
2014-11-11 18:38:24 +0800

    promise

--- Libraries/M9/M9Promise.h
+++ Libraries/M9/M9Promise.h
@@ -8,28 +8,68 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ * M9Promise
+ *  a simple implementation Promises/A+ with Objective-C
+ *
+ * @see
+ *  Promises/A+
+ *  https://promisesaplus.com/
+ *  http://www.ituring.com.cn/article/66566
+ *  PromiseJS Implementing
+ *  https://www.promisejs.org/
+ *  https://www.promisejs.org/implementing/
+ *  https://github.com/then/promise/
+ */
 @class M9Promise;
 
-typedef void (^Fulfill)(id value);
-typedef void (^Reject)(id reason);
-typedef void (^Task)(Fulfill fulfill, Reject reject);
+/**
+ * M9PromiseCallback
+ *  callback type for fulfill/reject
+ *
+ * @param id value
+ *  value/reason to fulfill/reject the current-promise
+ *
+ * @return
+ *  nil if nothing to do,
+ *  a value/reason to fulfill/reject the next promise from then,
+ *  or a new promise before the next-promise in the chain
+ */
+typedef id (^M9PromiseCallback)(id value);
 
-typedef M9Promise *(^Then)(Fulfill onFulfilled, Reject onRejected);
-typedef M9Promise *(^Done)(Fulfill onFulfilled);
-typedef M9Promise *(^Catch)(Reject onRejected);
+typedef M9Promise *(^M9Then)(M9PromiseCallback fulfillCallback, M9PromiseCallback rejectCallback);
+typedef M9Promise *(^M9Done)(M9PromiseCallback fulfillCallback);
+typedef M9Promise *(^M9Catch)(M9PromiseCallback rejectCallback);
+
+typedef NS_ENUM(NSInteger, M9PromiseState) {
+    M9PromiseStatePending = 0,
+    M9PromiseStateFulfilled,
+    M9PromiseStateRejected
+};
+
+#define M9PromiseError @"M9PromiseError"
+typedef NS_ENUM(NSInteger, M9PromiseErrorCode) {
+    M9PromiseErrorCode_TypeError
+};
+
+#define M9PromiseException @"M9PromiseException"
+
+#pragma mark -
 
 @interface M9Promise : NSObject
 
-+ (instancetype)promise:(Task)task;
-+ (instancetype)all:(Task)task, ...;
-+ (instancetype)any:(Task)task, ...;
+@property(nonatomic) M9PromiseState state;
 
-@property(nonatomic, copy, readonly) Then then/* (Fulfill onFulfilled, Reject onRejected) */;
-@property(nonatomic, copy, readonly) Done done/* (Fulfill onFulfilled) */;
-@property(nonatomic, copy, readonly) Catch catch/* (Reject onRejected) */;
+typedef void (^M9PromiseTask)(M9PromiseCallback fulfill, M9PromiseCallback reject);
++ (instancetype)promise:(M9PromiseTask)task;
 
-@property(nonatomic, copy, readonly) Fulfill fulfill;
-@property(nonatomic, copy, readonly) Reject reject;
+@property(nonatomic, copy, readonly) M9Then then/* (M9PromiseCallback fulfillCallback, M9PromiseCallback rejectCallback) */;
+@property(nonatomic, copy, readonly) M9Done done/* (M9PromiseCallback fulfillCallback) */;
+@property(nonatomic, copy, readonly) M9Catch catch/* (M9PromiseCallback rejectCallback) */;
+
+// ???: M9Promise or M9PromiseTask
++ (instancetype)all:(id)task, ...;
++ (instancetype)any:(id)task, ...;
 
 @end
 
@@ -39,7 +79,7 @@ typedef M9Promise *(^Catch)(Reject onRejected);
 
 @interface M9Promise (ObjC)
 
-- (instancetype)done:(Fulfill)done catch:(Reject)catch;
+- (instancetype)done:(M9PromiseCallback)done catch:(M9PromiseCallback)catch;
 
 @end
 
--- Libraries/M9/M9Promise.m
+++ Libraries/M9/M9Promise.m
@@ -11,67 +11,160 @@
 #import "EXTScope.h"
 
 #import "M9Utilities.h"
-#import "NSArray+.h"
-#import "NSDictionary+.h"
 
 @interface M9Promise ()
 
-@property(nonatomic, copy) Task task;
+@property(nonatomic, copy) M9PromiseTask task;
+@property(nonatomic, strong) id value;
+@property(nonatomic, strong) M9Promise *next;
+
+@property(nonatomic, copy, readonly) M9PromiseCallback fulfill;
+@property(nonatomic, copy, readonly) M9PromiseCallback reject;
+
+@property(nonatomic, strong) NSMutableArray *fulfillCallbacks, *rejectCallbacks;
 
 @end
 
-@implementation M9Promise {
-    Then _then;
-    Done _done;
-    Catch _catch;
+void resolveNext(M9Promise *nextPromise, id xValue) {
+    if (xValue == nextPromise) {
+        nextPromise.reject([NSError errorWithDomain:M9PromiseError code:M9PromiseErrorCode_TypeError userInfo:nil]);
+    }
     
-    Fulfill _fulfill;
-    Reject _reject;
+    if ([xValue isKindOfClass:[M9Promise class]]) {
+        M9Promise *xPromise = (M9Promise *)xValue;
+        if (xPromise.state == M9PromiseStateFulfilled) {
+            nextPromise.fulfill(xPromise.value);
+        }
+        else if (xPromise.state == M9PromiseStateRejected) {
+            nextPromise.reject(xPromise.value);
+        }
+        else if (xPromise.state == M9PromiseStatePending) {
+            xPromise.then(nextPromise.fulfill, nextPromise.reject);
+        }
+    }
+    else {
+        nextPromise.fulfill(xValue);
+    }
+};
+
+void fire(M9Promise *promise) {
+    NSArray *callbacks = nil;
+    if (promise.state == M9PromiseStateFulfilled) {
+        callbacks = promise.fulfillCallbacks;
+    }
+    else if (promise.state == M9PromiseStateRejected) {
+        callbacks = promise.rejectCallbacks;
+    }
+    id value = promise.value;
+    
+    for (id (^callback)(id value) in callbacks) {
+        resolveNext(promise.next, callback(value));
+    }
+    [promise.fulfillCallbacks removeAllObjects];
+    [promise.rejectCallbacks removeAllObjects];
+}
+
+@implementation M9Promise {
+    M9Then _then;
+    M9Done _done;
+    M9Catch _catch;
+    
+    M9PromiseCallback _fulfill;
+    M9PromiseCallback _reject;
+}
+
++ (instancetype)promise:(M9PromiseTask)task {
+    return [[self alloc] initWithTask:task];
 }
 
 - (instancetype)init {
+    return [self initWithTask:nil];
+}
+
+- (instancetype)initWithTask:(M9PromiseTask)task {
     self = [super init];
     if (self) {
         weakify(self);
         
-        _then = ^(Fulfill onFulfilled, Reject onRejected) {
+        self.task = [task copy];
+        
+        _then = ^M9Promise *(M9PromiseCallback fulfillCallback, M9PromiseCallback rejectCallback) {
             strongify(self);
-            return [[self class] promise:^(Fulfill fulfill, Reject reject) {
-                // TODO: handle(new Handler(onFulfilled, onRejected, resolve, reject))
-            }];
-        };
-        _done = ^(Fulfill onFulfilled) {
-            strongify(self);
-            return self.then(onFulfilled, nil);
-        };
-        _catch = ^(Reject onRejected) {
-            strongify(self);
-            return self.then(nil, onRejected);
+            
+            self.next = [M9Promise new];
+            
+            dispatch_async_main_queue(^() {
+                if (self.state == M9PromiseStateFulfilled) {
+                    resolveNext(self.next, fulfillCallback(self.value));
+                }
+                else if (self.state == M9PromiseStateRejected) {
+                    resolveNext(self.next, rejectCallback(self.value));
+                }
+                else if (self.state == M9PromiseStatePending) {
+                    if (fulfillCallback) {
+                        self.fulfillCallbacks = self.fulfillCallbacks OR [NSMutableArray new];
+                        [self.fulfillCallbacks addObject:[fulfillCallback copy]];
+                    }
+                    if (rejectCallback) {
+                        self.rejectCallbacks = self.rejectCallbacks OR [NSMutableArray new];
+                        [self.rejectCallbacks addObject:[rejectCallback copy]];
+                    }
+                }
+            });
+            
+            /* TODO:
+            self.next = [M9Promise promise:^(M9PromiseCallback fulfill, M9PromiseCallback reject) {
+            }]; */
+            
+            return self.next;
         };
         
-        _fulfill = ^void (id value) {
+        _done = ^M9Promise *(M9PromiseCallback fulfillCallback) {
+            strongify(self);
+            return self.then(fulfillCallback, nil);
         };
-        _reject = ^void (id reason) {
+        
+        _catch = ^M9Promise *(M9PromiseCallback rejectCallback) {
+            strongify(self);
+            return self.then(nil, rejectCallback);
         };
+        
+        _fulfill = ^id (id value) {
+            strongify(self);
+            if (self.state == M9PromiseStateRejected) {
+                @throw [NSException exceptionWithName:M9PromiseException reason:@"Illegal call." userInfo:nil];
+            }
+            self.state = M9PromiseStateFulfilled;
+            self.value = value;
+            fire(self);
+            return self;
+        };
+        
+        _reject = ^id (id reason) {
+            strongify(self);
+            if (self.state == M9PromiseStateFulfilled) {
+                @throw [NSException exceptionWithName:M9PromiseException reason:@"Illegal call." userInfo:nil];
+            }
+            self.state = M9PromiseStateRejected;
+            self.value = reason;
+            fire(self);
+            return self;
+        };
+        
+        task(self.fulfill, self.reject);
     }
     return self;
 }
 
-+ (instancetype)promise:(Task)task {
++ (instancetype)all:(id)first, ... {
     M9Promise *promise = [self new];
-    promise.task = task;
+    // TODO: NSMutableArray *tasks = va_array(id, first);
     return promise;
 }
 
-+ (instancetype)all:(Task)first, ... {
++ (instancetype)any:(id)first, ... {
     M9Promise *promise = [self new];
-    // TODO: NSMutableArray *tasks = va_array(M9Promise *, first);
-    return promise;
-}
-
-+ (instancetype)any:(Task)first, ... {
-    M9Promise *promise = [self new];
-    // TODO: NSMutableArray *tasks = va_array(M9Promise *, first);
+    // TODO: NSMutableArray *tasks = va_array(id, first);
     return promise;
 }
 
