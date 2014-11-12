7faba71ac431531a95a98ed4a5d1db7ef56bb3c4
MingLQ <minglq.9@gmail.com>
2014-11-12 17:00:02 +0800

    expecta

---
+++ M9DevTests/M9Promise/M9PromiseTests.m
@@ -0,0 +1,40 @@
+//
+//  M9PromiseTests.m
+//  M9Dev
+//
+//  Created by MingLQ on 2014-11-12.
+//  Copyright (c) 2014年 iwill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface M9PromiseTests : XCTestCase
+
+@end
+
+@implementation M9PromiseTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
--- Podfile
+++ Podfile
@@ -1,10 +1,12 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
 platform :ios, '7.0'
 
-pod 'AFNetworking', '~> 2.4'
-# pod 'AFNetworking', :git => 'https://github.com/iwill/AFNetworking.git', :commit => '5764f5400908b480bdb413b3b2a900916b62d7cc'
+# pod 'AFNetworking', '~> 2.4'
+pod 'AFNetworking', :git => 'https://github.com/iwill/AFNetworking.git', :commit => '5764f5400908b480bdb413b3b2a900916b62d7cc'
 
 pod 'TMCache', '~> 1.2'
 
-# pod 'SDWebImage', '~>3.7'
+pod 'SDWebImage', '~>3.7'
 
 pod 'Expecta', '~>0.3'
