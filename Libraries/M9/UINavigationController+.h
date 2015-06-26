742c0b51dbcd4e2d132274adb87d80eb0881cfeb
minglq <minglq.9@gmail.com>
2015-06-26 19:22:33 +0800

    nav+

---
+++ Libraries/M9/UINavigationController+.h
@@ -0,0 +1,41 @@
+//
+//  UINavigationController+.h
+//  iPhoneVideo
+//
+//  Created by MingLQ on 2011-11-15.
+//  Copyright 2011 SOHU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINavigationController (M9Category)
+
+@property(nonatomic, strong, readonly) UIViewController *rootViewController;
+
+// !!!: the delegate and interactivePopGestureRecognizer.delegate of the returned navigationController is itself
++ (UINavigationController *)navigationControllerWithRootViewController:(UIViewController *)rootViewController;
+
+// push&pop completion
+- (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated completion:(void (^)(void))completion;
+- (UIViewController *)popViewControllerAnimated:(BOOL)animated completion:(void (^)(void))completion;
+- (NSArray *)popToViewController:(UIViewController *)viewController animated:(BOOL)animated completion:(void (^)(void))completion;
+- (NSArray *)popToRootViewControllerAnimated:(BOOL)animated completion:(void (^)(void))completion;
+
+// for performSelector:@selector(popViewController<#Animated#>/<#NonAnimated#>)
+- (UIViewController *)popViewControllerAnimated;
+- (UIViewController *)popViewControllerNonAnimated;
+
+@end
+
+#pragma mark -
+
+@interface UIViewController (UINavigationController)
+
+// !!!: @see - [UINavigationController navigationControllerWithRootViewController:self];
+- (UINavigationController *)wrapWithNavigationController;
+
+- (UIViewController *)popFromNavigationControllerAnimated;
+- (UIViewController *)popFromNavigationControllerNonAnimated;
+
+@end
+
