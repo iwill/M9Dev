5008992a0563da09e00526326eb3e6c6bf2ed92f
iwill <minglq.9@gmail.com>
2015-08-20 20:04:32 +0800

    M9Dev.h

--- M9Dev/M9Dev.h
+++ M9Dev/M9Dev.h
@@ -6,7 +6,24 @@
 //  Copyright (c) 2015年 MingLQ <minglq.9@gmail.com>. All rights reserved.
 //
 
-#ifndef M9Dev_M9Dev_h
-#define M9Dev_M9Dev_h
+#ifndef M9Dev_h
+#define M9Dev_h
+
+#improt "NSObject+AssociatedObjects.h"
+#improt "NSString+CompareToVersion.h"
+#improt "EXTScope+M9.h"
+#improt "metamacros+M9.h"
+#improt "JRSwizzle.h"
+
+#improt "M9PagingViewController.h"
+#improt "M9ScrollViewController.h"
+#improt "M9Utilities.h"
+#improt "NSArray+M9.h"
+#improt "NSDictionary+M9.h"
+#improt "NSObject+AssociatedValues.h"
+#improt "UIControl+M9EventCallback.h"
+#improt "UINavigationController+M9.h"
+#improt "UIView+M9.h"
+#improt "UIViewController+M9.h"
 
 #endif
