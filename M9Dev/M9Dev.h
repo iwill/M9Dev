6b18b6d935d7e319662f4b0e14df627b0afb0f47
iwill <minglq.9@gmail.com>
2015-09-06 20:45:56 +0800

    update

--- M9Dev/M9Dev.h
+++ M9Dev/M9Dev.h
@@ -17,21 +17,31 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+#import "M9Utilities.h"
+
+/* third part */
 #import "NSObject+AssociatedObjects.h"
 #import "NSString+CompareToVersion.h"
 #import "EXTScope+M9.h"
 #import "metamacros+M9.h"
 #import "JRSwizzle.h"
 
-#import "M9PagingViewController.h"
-#import "M9ScrollViewController.h"
-#import "M9Utilities.h"
+/* Foundation */
 #import "NSArray+M9.h"
 #import "NSDictionary+M9.h"
+#import "NSInvocation+M9.h"
 #import "NSObject+AssociatedValues.h"
+
+/* UIKit */
+#import "UIImage+M9.h"
+// view
 #import "UIControl+M9EventCallback.h"
-#import "UINavigationController+M9.h"
 #import "UIView+M9.h"
+// view controller
+#import "M9ScrollViewController.h"
+#import "M9TableViewController.h"
+#import "M9PagingViewController.h"
+#import "UINavigationController+M9.h"
 #import "UIViewController+M9.h"
 
 #endif
