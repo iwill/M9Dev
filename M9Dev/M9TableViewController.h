6a0a4fe4e7a95c6f7dba1005116532a5c996d89b
iwill <minglq.9@gmail.com>
2015-09-06 14:46:36 +0800

    update

---
+++ M9Dev/M9TableViewController.h
@@ -0,0 +1,25 @@
+//
+//  M9TableViewController.h
+//  M9Dev
+//
+//  Created by MingLQ on 2015-05-12.
+//  Copyright (c) 2015 MingLQ <minglq.9@gmail.com>.
+//  Released under the MIT license.
+//
+
+#import "M9ScrollViewController.h"
+
+@interface M9TableViewController : M9ScrollViewController {
+@protected
+    UITableView *_tableView;
+}
+
+// UITableViewStylePlain by default
+@property(nonatomic, readonly) UITableViewStyle style;
+// !!!: dataSource & delegate are nil by default
+@property(nonatomic, strong, readonly) UITableView *tableView;
+@property(nonatomic) BOOL clearsSelectionOnViewWillAppear;
+
+- (instancetype)initWithStyle:(UITableViewStyle)style NS_DESIGNATED_INITIALIZER;
+
+@end
---
+++ M9Dev/M9TableViewController.m
@@ -0,0 +1,79 @@
+//
+//  M9TableViewController.m
+//  M9Dev
+//
+//  Created by MingLQ on 2015-05-12.
+//  Copyright (c) 2015 MingLQ <minglq.9@gmail.com>.
+//  Released under the MIT license.
+//
+
+#import "M9TableViewController.h"
+
+@interface M9TableViewController ()
+
+@property(nonatomic, readwrite) UITableViewStyle style;
+@property(nonatomic, strong, readwrite) UITableView *tableView;
+
+@end
+
+@implementation M9TableViewController
+
+- (instancetype)init {
+    return [self initWithStyle:UITableViewStylePlain];
+}
+
+- (instancetype)initWithStyle:(UITableViewStyle)style {
+    self = [super init];
+    if (self) {
+        self.style = style;
+    }
+    return self;
+}
+
+@dynamic scrollView;
+- (UIScrollView *)scrollView {
+    return self.tableView;
+}
+
+@synthesize tableView = _tableView;
+- (UITableView *)tableView {
+    if (![self isViewLoaded]) {
+        [self view];
+    }
+    if (_tableView) {
+        return _tableView;
+    }
+    
+    UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+    // tableView.delegate = self;
+    // tableView.dataSource = self;
+    self.tableView = tableView;
+    [self.view addSubview:tableView];
+    
+    [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.top.width.height.equalTo(self.view);
+    }];
+    return _tableView;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self tableView];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    if (self.clearsSelectionOnViewWillAppear) {
+        NSArray *indexPaths = [self.tableView indexPathsForSelectedRows];
+        for (NSIndexPath *indexPath in indexPaths) {
+            [self.tableView deselectRowAtIndexPath:indexPath animated:animated];
+        }
+    }
+}
+
+- (void)dealloc {
+    self.tableView.dataSource = nil;
+    self.tableView.delegate = nil;
+}
+
+@end
