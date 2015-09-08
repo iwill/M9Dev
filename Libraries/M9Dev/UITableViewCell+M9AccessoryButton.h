0b7aad9694d4b90913fb505a6dac614eb6ab6933
iwill <minglq.9@gmail.com>
2015-09-08 20:59:58 +0800

    UITableViewCell+M9AccessoryButton

---
+++ Libraries/M9Dev/UITableViewCell+M9AccessoryButton.h
@@ -0,0 +1,18 @@
+//
+//  UITableViewCell+M9AccessoryButton.h
+//  M9Dev
+//
+//  Created by MingLQ on 2015-09-08.
+//  Copyright (c) 2015年 MingLQ <minglq.9@gmail.com>. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "M9Utilities.h"
+#import "UIView+M9.h"
+
+@interface UITableViewCell (M9AccessoryButton)
+
+@property (nonatomic, strong) UIButton *accessoryButton;
+
+@end
---
+++ Libraries/M9Dev/UITableViewCell+M9AccessoryButton.m
@@ -0,0 +1,33 @@
+//
+//  UITableViewCell+M9AccessoryButton.m
+//  M9Dev
+//
+//  Created by MingLQ on 2015-09-08.
+//  Copyright (c) 2015年 MingLQ <minglq.9@gmail.com>. All rights reserved.
+//
+
+#import "UITableViewCell+M9AccessoryButton.h"
+
+@implementation UITableViewCell (M9AccessoryButton)
+
+@dynamic accessoryButton;
+
+- (UIButton *)accessoryButton {
+    return [self.accessoryView as:[UIButton class]];
+}
+
+- (void)setAccessoryButton:(UIButton *)accessoryButton {
+    self.accessoryView = accessoryButton;
+    [accessoryButton addTarget:self action:@selector(m9_accessoryButtonTappedWithButton:) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void)m9_accessoryButtonTappedWithButton:(UIButton *)button {
+    UITableViewCell *cell = (UITableViewCell *)[button closestViewOfClass:[UITableViewCell class]];
+    UITableView *tableView = (UITableView *)[cell closestViewOfClass:[UITableView class]];
+    NSIndexPath *indexPath = [tableView indexPathForCell:cell];
+    if (indexPath && [tableView.delegate respondsToSelector:@selector(tableView:accessoryButtonTappedForRowWithIndexPath:)]) {
+        [tableView.delegate tableView:tableView accessoryButtonTappedForRowWithIndexPath:indexPath];
+    }
+}
+
+@end
