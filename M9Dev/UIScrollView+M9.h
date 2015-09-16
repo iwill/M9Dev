a4bb1038f8055dc50fda5076934cd2d2fd896421
iwill <minglq.9@gmail.com>
2015-09-16 20:53:50 +0800

    支持滚动刷新/加载更多

---
+++ M9Dev/UIScrollView+M9.h
@@ -0,0 +1,25 @@
+//
+//  UIScrollView+M9.h
+//  M9Dev
+//
+//  Created by MingLQ on 2014-09-04.
+//  Copyright (c) 2014 MingLQ <minglq.9@gmail.com>.
+//  Released under the MIT license.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScrollView (UIScrollViewScrolledToTheBottomEdge)
+
+- (BOOL)scrolledToTheBottomEdge;
+
+/**
+ *  pulling down to reload: bouncingOffsetY < 0
+ *  scrolling up to load more: bouncingOffsetY > 0
+ *  otherwise: bouncingOffsetY == 0
+ */
+- (CGFloat)bouncingOffsetY;
+- (BOOL)isPullingDownToReload;
+- (BOOL)isScrollingDownToReload;
+
+@end
---
+++ M9Dev/UIScrollView+M9.m
@@ -0,0 +1,68 @@
+//
+//  UIScrollView+M9.m
+//  M9Dev
+//
+//  Created by MingLQ on 2014-09-04.
+//  Copyright (c) 2014 MingLQ <minglq.9@gmail.com>.
+//  Released under the MIT license.
+//
+
+#import "UIScrollView+M9.h"
+
+@implementation UIScrollView (UIScrollViewScrolledToTheBottomEdge)
+
+- (BOOL)scrolledToTheBottomEdge {
+    if (!self.dragging && !self.decelerating) {
+        return NO;
+    }
+    if (self.contentOffset.y <= - self.contentInset.top) {
+        return NO;
+    }
+    
+    CGFloat bottomOffset = self.contentOffset.y + self.frame.size.height - self.contentSize.height;
+    CGFloat bottomViewHeight = 0;
+    return bottomOffset - bottomViewHeight >= 0;
+}
+
+- (CGFloat)bouncingOffsetY {
+    CGFloat top = self.contentInset.top;
+    CGFloat bottom = self.contentInset.bottom;
+    CGFloat viewHeight = CGRectGetHeight(self.frame);
+    CGFloat contentOffset = self.contentOffset.y;
+    CGFloat contentHeight = self.contentSize.height;
+    
+    CGFloat topOffset = contentOffset + top;
+    CGFloat bottomOffset = contentOffset + viewHeight - bottom - contentHeight;
+    
+    if (topOffset < 0.0) {
+        NSLog(@"topOffset: %f", topOffset);
+        return topOffset;
+    }
+    else if (topOffset > 0 && bottomOffset >= 0) {
+        CGFloat minContentHeight = top + viewHeight + bottom;
+        if (contentHeight < minContentHeight) {
+            NSLog(@"bottomOffset: %f", topOffset);
+            return topOffset;
+        }
+        else {
+            NSLog(@"bottomOffset: %f", bottomOffset);
+            return bottomOffset;
+        }
+    }
+    else {
+        return 0.0;
+    }
+}
+
+- (BOOL)isPullingDownToReload {
+    if (!self.dragging) {
+        return NO;
+    }
+    return [self bouncingOffsetY] < 0.0;
+}
+
+- (BOOL)isScrollingDownToReload {
+    return [self bouncingOffsetY] > 0.0;
+}
+
+@end
