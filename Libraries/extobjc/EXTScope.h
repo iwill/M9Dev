857b1801d0f2651424731c98091003de959d330c
iwill <minglq.9@gmail.com>
2015-09-19 00:06:06 +0800

    revert ext

--- Libraries/extobjc/EXTScope.h
+++ Libraries/extobjc/EXTScope.h
@@ -7,10 +7,30 @@
 //  Released under the MIT license.
 //
 
-#ifndef EXTC_EXTSCOPE_H
-#define EXTC_EXTSCOPE_H
-
 #import "metamacros.h"
+
+/**
+ * \@onExit defines some code to be executed when the current scope exits. The
+ * code must be enclosed in braces and terminated with a semicolon, and will be
+ * executed regardless of how the scope is exited, including from exceptions,
+ * \c goto, \c return, \c break, and \c continue.
+ *
+ * Provided code will go into a block to be executed later. Keep this in mind as
+ * it pertains to memory management, restrictions on assignment, etc. Because
+ * the code is used within a block, \c return is a legal (though perhaps
+ * confusing) way to exit the cleanup block early.
+ *
+ * Multiple \@onExit statements in the same scope are executed in reverse
+ * lexical order. This helps when pairing resource acquisition with \@onExit
+ * statements, as it guarantees teardown in the opposite order of acquisition.
+ *
+ * @note This statement cannot be used within scopes defined without braces
+ * (like a one line \c if). In practice, this is not an issue, since \@onExit is
+ * a useless construct in such a case anyways.
+ */
+#define onExit \
+    ext_keywordify \
+    __strong ext_cleanupBlock_t metamacro_concat(ext_exitBlock_, __LINE__) __attribute__((cleanup(ext_executeCleanupBlock), unused)) = ^
 
 /**
  * Creates \c __weak shadow variables for each of the variables provided as
@@ -23,6 +43,7 @@
  * See #strongify for an example of usage.
  */
 #define weakify(...) \
+    ext_keywordify \
     metamacro_foreach_cxt(ext_weakify_,, __weak, __VA_ARGS__)
 
 /**
@@ -30,6 +51,7 @@
  * classes that do not support weak references.
  */
 #define unsafeify(...) \
+    ext_keywordify \
     metamacro_foreach_cxt(ext_weakify_,, __unsafe_unretained, __VA_ARGS__)
 
 /**
@@ -45,13 +67,13 @@
     id foo = [[NSObject alloc] init];
     id bar = [[NSObject alloc] init];
 
-    weakify(foo, bar);
+    @weakify(foo, bar);
 
     // this block will not keep 'foo' or 'bar' alive
     BOOL (^matchesFooOrBar)(id) = ^ BOOL (id obj){
         // but now, upon entry, 'foo' and 'bar' will stay alive until the block has
         // finished executing
-        strongify(foo, bar);
+        @strongify(foo, bar);
 
         return [foo isEqual:obj] || [bar isEqual:obj];
     };
@@ -59,12 +81,16 @@
  * @endcode
  */
 #define strongify(...) \
+    ext_keywordify \
     _Pragma("clang diagnostic push") \
     _Pragma("clang diagnostic ignored \"-Wshadow\"") \
     metamacro_foreach(ext_strongify_,, __VA_ARGS__) \
     _Pragma("clang diagnostic pop")
 
 /*** implementation details follow ***/
+typedef void (^ext_cleanupBlock_t)();
+
+void ext_executeCleanupBlock (__strong ext_cleanupBlock_t *block);
 
 #define ext_weakify_(INDEX, CONTEXT, VAR) \
     CONTEXT __typeof__(VAR) metamacro_concat(VAR, _weak_) = (VAR);
@@ -72,4 +98,19 @@
 #define ext_strongify_(INDEX, VAR) \
     __strong __typeof__(VAR) VAR = metamacro_concat(VAR, _weak_);
 
+// Details about the choice of backing keyword:
+//
+// The use of @try/@catch/@finally can cause the compiler to suppress
+// return-type warnings.
+// The use of @autoreleasepool {} is not optimized away by the compiler,
+// resulting in superfluous creation of autorelease pools.
+//
+// Since neither option is perfect, and with no other alternatives, the
+// compromise is to use @autorelease in DEBUG builds to maintain compiler
+// analysis, and to use @try/@catch otherwise to avoid insertion of unnecessary
+// autorelease pools.
+#if DEBUG
+#define ext_keywordify autoreleasepool {}
+#else
+#define ext_keywordify try {} @catch (...) {}
 #endif
