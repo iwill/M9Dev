3a9a180b837d1d8e875caa1ad68fd6c9b3f11941
iwill <minglq.9@gmail.com>
2015-11-17 15:52:40 +0800

    M9PagingViewController 支持 scrollViewInsets

--- M9Dev/M9PagingViewController.h
+++ M9Dev/M9PagingViewController.h
@@ -22,21 +22,33 @@
 @property(nonatomic, readonly) NSInteger currentPage;
 
 /**
- *  call setupWithNumberOfPages: when viewDidLoad
+ *  call when subclasses - viewDidLoad
  */
 - (void)setupWithNumberOfPages:(NSInteger)numberOfPages;
+
 - (UIViewController *)viewControllerOfPage:(NSInteger)page;
-
-// subclasses MUST override
-// automaticallyAdjustsScrollViewInsets of the generated viewControllers will be set to NO
-- (UIViewController *)generateViewControllerOfPage:(NSInteger)page;
-// default: UIEdgeInsetsMake(0, 0, 0, 0)
-- (UIEdgeInsets)viewInsetsOfPage:(NSInteger)page;
-
 - (void)scrollToPage:(NSInteger)page animated:(BOOL)animated;
 
-// TODO: scroll progress - 1, 1.1, 1.2 ..., 2
+#pragma mark - subclasses MUST override
+
+/**
+ *  automaticallyAdjustsScrollViewInsets of the generated viewControllers will be set to NO
+ */
+- (UIViewController *)generateViewControllerOfPage:(NSInteger)page;
+
+#pragma mark - subclasses can override
+
+/**
+ *  default: UIEdgeInsetsMake(0, 0, 0, 0)
+ */
+- (UIEdgeInsets)viewInsetsOfPage:(NSInteger)page;
+/**
+ *  default: UIEdgeInsetsMake(self.topLayoutGuide.length, 0, self.bottomLayoutGuide.length, 0)
+ */
+- (UIEdgeInsets)scrollViewInsets;
+
 - (void)willScrollToPage:(NSInteger)page animated:(BOOL)animated;
 - (void)didScrollToPage:(NSInteger)page animated:(BOOL)animated;
+// TODO: scroll progress - 1, 1.1, 1.2 ..., 2
 
 @end
