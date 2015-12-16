fc640b05040c7d4f774e0cac3be1690e1c03cd3e
iwill <minglq.9@gmail.com>
2015-12-16 10:12:58 +0800

    thread suspend & resume

---
+++ M9Dev/NSThread+M9.h
@@ -0,0 +1,16 @@
+//
+//  NSThread+M9.h
+//  M9Dev
+//
+//  Created by MingLQ on 2015-12-15.
+//  Copyright © 2015年 MingLQ <minglq.9@gmail.com>. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSThread (M9)
+
+- (BOOL)suspend;
+- (void)resume;
+
+@end
---
+++ M9Dev/NSThread+M9.m
@@ -0,0 +1,35 @@
+//
+//  NSThread+M9.m
+//  M9Dev
+//
+//  Created by MingLQ on 2015-12-15.
+//  Copyright © 2015年 MingLQ <minglq.9@gmail.com>. All rights reserved.
+//
+
+#import "NSThread+M9.h"
+
+@implementation NSThread (M9)
+
+- (BOOL)suspend {
+    if ([self isMainThread]) {
+        return NO;
+    }
+    if (CFRunLoopCopyCurrentMode(CFRunLoopGetCurrent())) {
+        return NO;
+    }
+    NSLog(@"%s %@", __PRETTY_FUNCTION__, self);
+    CFRunLoopRun();
+    return YES;
+}
+
+- (void)resume {
+    if ([NSThread currentThread] == self) {
+        NSLog(@"%s %@", __PRETTY_FUNCTION__, self);
+        CFRunLoopStop(CFRunLoopGetCurrent());
+    }
+    else {
+        [self performSelector:_cmd onThread:self withObject:nil waitUntilDone:YES];
+    }
+}
+
+@end
