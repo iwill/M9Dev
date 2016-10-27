d2f9be97c484e91a1f903e3997626b82bda2441b
iwill <minglq.9@gmail.com>
2016-10-27 10:49:53 +0800

    shell

--- podspec.sh
+++ podspec.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-pod spec lint M9Dev.podspec \
-    --use-libraries \
-    --allow-warnings \
-    --verbose \
-&& \
-pod trunk push M9Dev.podspec \
-    --use-libraries \
-    --allow-warnings \
-    --verbose
+if [[ $1 = "-l" || $1 = "-lint" ]]; then
+    shift
+    pod spec lint M9Dev.podspec \
+        --use-libraries \
+        --allow-warnings \
+        --verbose
+else
+    pod trunk push M9Dev.podspec \
+        --use-libraries \
+        --allow-warnings \
+        --verbose
+fi
 
